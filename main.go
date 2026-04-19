05ba614e26359a9a83d7163a61dfb61e55dc71b0
dhana123-m <dhanasekar123rit@gmail.com>
2026-04-19 10:55:27 +0530

    Update main.go

--- main.go
+++ main.go
@@ -27,8 +27,8 @@ func main() {
 	// CORS FIX
 	r.Use(func(c *gin.Context) {
 		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type")
-		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(200)
